c4ea137300683033121433825780c4419c203695
yaffa-yaso <yaffa2411@gmail.com>
2020-01-15 23:54:35 +0200

    update

---
+++ MyClientHandler.cpp
@@ -0,0 +1,66 @@
+//
+// Created by omer on 1/15/20.
+//
+
+
+#include "General.h"
+
+
+MyClientHandler<P,S>::MyClientHandler(const Solver<matrix, matrix> &sol, const CacheManager<matrix, matrix> cacheManager):
+        solver(sol), cache(cacheManager) {}
+
+
+void MyTestClientHandler<matrix, matrix>::handleClient(int socket) {
+
+    char buffer[1024] = {0};
+    matrix problem;
+    matrix solution;
+    string message = " ";
+
+    while (read(client_socket, buffer, 1024)) {
+        char delimiter = ',';
+        int m = 0;
+        int j = 0;
+        if (!strcmp(buffer, "end")) {
+            break;
+        }
+
+        vector<char> v;
+        for (int i = 0; buffer[i] != '\n'; i++) {
+            // find next value
+            while ((buffer[j] != delimiter) && (buffer[j] != '\0')) {
+                j++;
+            }
+            // separate value from the rest of the string
+            string token = "";
+            while (m < j) {
+                token += buffer[m];
+                m++;
+            }
+            m++;
+
+            v.push_back(strtof((string(token)).c_str(), 0));
+
+            j++;
+        }
+
+        problem.push_back(v);
+    }
+
+    //searching for solution in the cache
+    if (buffer.inCache) {
+        solution = cacheManager.solution(problem);
+        //if solution wasn't found create one
+    } else {
+        solution = solver.solve(problem);
+        cache.intoCache(problem, solution);
+    }
+
+    for (int i = 0; i < solution.size; i++) {
+        for (int j = 0; j < solution[i].size; j++) {
+            message += solution[j];
+        }
+    }
+
+    write(socket, message.c_str(), message.length());
+}
