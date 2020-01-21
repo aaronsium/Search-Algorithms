9bb6f5f3c4dcf64516f302cd07fd45654380abe1
aaronsium <aaronsm132@gmail.com>
2020-01-21 15:50:55 +0200

    update

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 #include "General.h"
 
 
-int main() {
-
-  std::cout << "Hello, World!" << std::endl;
+int main(int arg, char *args[]) {
+  MySerialServer server;
+ OA <matrix,matrix,Point> oa = ("הבעיה",BFS<Point,vector<State<Point>> );
+  FileCacheManager<matrix,string> *cash;
+  ClientHandler *client = MyTestClientHandler(oa,cash)
+  stringstream geek(args[0]);
+  int port = 0;
+  geek >> port;
+  server.open(port,client);
   return 0;
 }
