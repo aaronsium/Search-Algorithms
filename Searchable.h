d0a25d53b9ec485baa19f00e1748dfcd03e6d3f3
yaffa-yaso <yaffa2411@gmail.com>
2020-01-24 00:43:56 +0200

    update

---
+++ Searchable.h
@@ -0,0 +1,51 @@
+//
+// Created by omer on 1/24/20.
+//
+
+#ifndef EX4_SEARCHABLE_H
+#define EX4_SEARCHABLE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <bits/stdc++.h>
+#include <arpa/inet.h>
+#include <fstream>
+#include <algorithm>
+#include <cstring>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <cstddef>
+#include <stdlib.h>
+#include <string>
+#include <functional>
+
+using namespace std;
+using namespace std::literals::chrono_literals;
+typedef vector<vector<int>> matrix;
+typedef vector<string> strVector;
+#include "Searchable.h"
+#include "Point.h"
+#include "State.h"
+
+template<class T>
+class Searchable {
+protected:
+    State<Point> initial;
+    State<Point> goal;
+public:
+    Searchable();
+    virtual State<T> getInitialState() = 0;
+    virtual bool isGoalState(State<T>) = 0;
+    virtual list<State<T>> getAllPossibleStates(State<T> current) = 0;
+    virtual int NodesEvaluated() = 0;
+    virtual vector<string> adaptSolution(vector<State<Point>> stateVector)=0;
+    virtual ~Searchable() = default;
+};
+
+#endif //EX4_SEARCHABLE_H
