1a3de6b34193ff8a2a1ecf3b724c027e2b3e6827
aaronsium <aaronsm132@gmail.com>
2020-01-24 00:39:14 +0200

    update

--- FileCacheManager.h
+++ FileCacheManager.h
@@ -5,6 +5,16 @@
 #ifndef EX4__FILECACHEMANAGER_H_
 #define EX4__FILECACHEMANAGER_H_
 #include "CacheManager.h"
+#include <string>
+#include <list>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
 template<class S>
 class FileCacheManager : public CacheManager<string, S> {
  private:
