53bb79b532afa005a8de5bdde745b8ff13e62627
yaffa-yaso <yaffa2411@gmail.com>
2020-01-24 01:27:40 +0200

    update

--- MyClaientHandler.h
+++ MyClaientHandler.h
@@ -5,95 +5,21 @@
 #ifndef EX4__MYCLAIENTHANDLER_H_
 #define EX4__MYCLAIENTHANDLER_H_
 
-#include "ClientHandler.h"
 #include "Solver.h"
-#include "CacheManager.h"
-#include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <unistd.h>
-#include <cstring>
-
-using namespace std;
-
 
 typedef vector<vector<int>> matrix;
 
 
 class MyClientHandler : public ClientHandler {
 
- private:
-  Solver<matrix, vector<string>>* solver;
-  CacheManager<string, vector<string>> *cache;
+private:
+    Solver<matrix, vector<string>>* solver;
+    CacheManager<string, vector<string>> *cache;
 
- public:
-  ~MyClientHandler() {}
-
-  //////////////////////////////////////////////////
-
-  void handleClient(int socket) {
-
-    char buffer[1024] = {0};
-    matrix problem;
-    vector<string> solution;
-    string message = " ";
-    string strProblem = " ";
-    int colRow = 3;
-    int lineNum = 0;
-
-    while ((read(socket, buffer, 1024)) && (colRow > 0)) {
-      char delimiter = ',';
-      int m = 0;
-      int j = 0;
-      if (!strcmp(buffer, "end")) {
-        colRow--;
-      } else {
-        if (colRow < 3) {
-          colRow--;
-        }
-
-        for (int i = 0; buffer[i] != '\n'; i++) {
-          vector<int> v;
-          // find next value
-          while ((buffer[j] != delimiter) && (buffer[j] != '\0')) {
-            j++;
-          }
-          // separate value from the rest of the string
-          string token = "";
-          while (m < j) {
-            token += buffer[m];
-            m++;
-          }
-          m++;
-          if(lineNum == 0) {
-            v.push_back(stoi(token));
-            problem.push_back(v);
-          } else{
-            problem[i].push_back(stoi(token));
-          }
-          j++;
-          strProblem += token + " ";
-        }
-      }
-      lineNum ++;
-    }
-
-    //searching for solution in the cache
-    if (this->cache->inCache(strProblem)) {
-      solution = this->cache->getSolution(strProblem);
-      //if solution wasn't found create one
-    } else {
-      solution = this->solver->solve(problem);
-      this->cache->intoCache(strProblem, solution);
-    }
-
-    for (int i = 0; i < solution.size(); i++) {
-      message = message + solution[i];
-    }
-
-    write(socket, message.c_str(), message.length());
-  }
+public:
+    MyClientHandler(Solver<matrix, vector<string>>* sol, CacheManager<string, vector<string>>* cacheManager): solver(sol),
+                                                                                                              cache(cacheManager){};
+    void handleClient(int socket);
+    ~MyClientHandler() {}
 };
-
 #endif //EX4__MYCLAIENTHANDLER_H_
