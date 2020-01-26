3800d90ebf809f6221db0e7db5a8b9a3ec8698a5
yaffa-yaso <yaffa2411@gmail.com>
2020-01-26 02:10:47 +0200

    update

--- BestFirstSearch.h
+++ BestFirstSearch.h
@@ -21,19 +21,18 @@ class BestFirstSearch : public Searcher<T, S> {
   virtual ~BestFirstSearch() = default;
   ///////////////////////////////////////////////
   S search(Searchable<T>* searchable) {
-    priority_queue<State<T>> openList;
-    openList.push(searchable->getInitialState());
-    while (!openList.empty()) {
-      State<T> n = openList.top();
+    this->openList.push(searchable->getInitialState());
+    while (!this->openList.empty()) {
+      State<T> n = this->openList.top();
       this->openList.pop();
-      closed.push_back(n);
+      this->closed.push_back(n);
       if (searchable->isGoalState(n)) {
         return searchable->adaptSolution(backTrace());
       }
 
-      while (!openList.empty()){
-        opened.push_back(openList.top());
-        openList.pop();
+      while (!this->openList.empty()){
+        this->opened.push_back(this->openList.top());
+        this->openList.pop();
       }
 
       list<State<T>> options = searchable->getAllPossibleStates(n);
@@ -42,7 +41,7 @@ class BestFirstSearch : public Searcher<T, S> {
         this->SetEvaluatedNodes(this->evaluatedNodes + 1);
         //iterator to check if option in opened
         typename list<State<T>>::iterator inOpen;
-        for (inOpen = opened.begin(); inOpen != opened.end(); ++inOpen){
+        for (inOpen = this->opened.begin(); inOpen != this->opened.end(); ++inOpen){
           if(inOpen->equals(*opt)) {
             break;
           }
@@ -51,28 +50,28 @@ class BestFirstSearch : public Searcher<T, S> {
         //iterator to check if option in closed
 //      for (std::list<T>::iterator it = mylist.begin(); it != mylist.end(); ++it)
         typename list<State<T>>::iterator inClosed;
-        for (inClosed = closed.begin(); inClosed != closed.end(); ++inClosed){
+        for (inClosed = this->closed.begin(); inClosed != this->closed.end(); ++inClosed){
           if(inClosed->equals(*opt)) {
             break;
           }
         }
 
-        if((inOpen == opened.end()) && (inClosed == closed.end())){
+        if((inOpen == this->opened.end()) && (inClosed == this->closed.end())){
           opt->SetCameFrom(&n);
-          openList.push(*opt);
-        } else if(inClosed != closed.end()){
+          this->openList.push(*opt);
+        } else if(inClosed != this->closed.end()){
           if(opt->GetCost() < inClosed->GetCost()){
-            if (inOpen != opened.end()) {
+            if (inOpen != this->opened.end()) {
               //popping and pushing again(for the priority process)
-              opened.remove(*inOpen);
+              this->opened.remove(*inOpen);
             }
-            openList.push(*opt);
-            closed.remove(*inClosed);
+            this->openList.push(*opt);
+            this->closed.remove(*inClosed);
           }
         } else {
           if(opt->GetCost() < inOpen->GetCost()) {
-            opened.remove(*inOpen);
-            opened.push_back(*opt);
+            this->opened.remove(*inOpen);
+            this->opened.push_back(*opt);
           }
         }
       }
