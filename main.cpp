addee4b5c9399bac0ae5807decd09e2c727f1093
yaffa-yaso <yaffa2411@gmail.com>
2020-01-30 00:57:56 +0200

    update

--- main.cpp
+++ main.cpp
@@ -15,21 +15,67 @@
 #include "MyParallelServer.h"
 
 int main(int arg, char *args[]) {
-  MySerialServer server;
+  MyParallelServer server;
 
-  Searcher<Point,vector<string>> *search;
-  search = new AStar<Point,vector<string>> ();
+  Searcher<Point, vector<string>> *search;
+  search = new BFS<Point, vector<string>>();
 
-  Solver<Matrix,vector<string>> *solv;
-  solv = new OA<Matrix,vector<string>,Point>(search);
+  Solver<Matrix, vector<string>> *solv;
+  solv = new OA<Matrix, vector<string>, Point>(search);
 
-  CacheManager<string,vector<string>> *cash2;
-  cash2 = new FileCacheManager<vector<string>> ();
+  CacheManager<string, vector<string>> *cash2;
+  cash2 = new FileCacheManager<vector<string>>();
+  vector<ClientHandler*>* handlers =new vector<ClientHandler*>();
 
-  MyClientHandler client(solv,cash2);
+  MyClientHandler client1(solv, cash2);
 
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client2(solv, cash2);
 
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client3(solv, cash2);
 
-  server.open(atoi(args[1]),&client);
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client4(solv, cash2);
+
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client5(solv, cash2);
+
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client6(solv, cash2);
+
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client7(solv, cash2);
+
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client8(solv, cash2);
+
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client9(solv, cash2);
+
+  search = new BFS<Point, vector<string>>();
+  solv = new OA<Matrix, vector<string>, Point>(search);
+  MyClientHandler client10(solv, cash2);
+
+  handlers->push_back(&client1);
+  handlers->push_back(&client2);
+  handlers->push_back(&client3);
+  handlers->push_back(&client4);
+  handlers->push_back(&client5);
+  handlers->push_back(&client6);
+  handlers->push_back(&client7);
+  handlers->push_back(&client8);
+  handlers->push_back(&client9);
+  handlers->push_back(&client10);
+
+  server.open(atoi(args[1]),handlers);
   return 0;
 }
