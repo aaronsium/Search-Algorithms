5d93e803de98c1ea3898b9ea14b5e69b9b8ba110
yaffa-yaso <yaffa2411@gmail.com>
2020-01-30 00:59:11 +0200

    update

--- MyParallelServer.h
+++ MyParallelServer.h
@@ -29,92 +29,89 @@
 #include "server_side"
 
 class MyParallelServer : public server_side::Server {
- private:
-  bool isOpen = false;
-  int PORT = 0;
+private:
+    bool isOpen = false;
+    int PORT = 0;
 //  int client_socket = 0;
-  int server_socket = 0;
-  int liveCounter=0;
-  vector<thread> *treadVector = new vector<thread>();
-  sockaddr_in address = {0};
-  int temp_socket=0;
- public:
-  void open(int port, ClientHandler *myHandler) {
-    //opening a new thread which will run the server
-    for(int i=0; i<10;i++) {
+    int server_socket = 0;
+    int liveCounter=0;
+    vector<thread> *treadVector = new vector<thread>();
+    sockaddr_in address = {0};
+    int temp_socket=0;
+public:
+    void open(int port, vector<ClientHandler*>* handlers) {
+      //opening a new thread which will run the server
+      newSocket(port,handlers);
 
-      // copy myHandler
-      ClientHandler *newHandler = myHandler->clone();
 
-      treadVector->push_back(thread(&MyParallelServer::start, this, &(*newHandler)));
-    }
-    newSocket(port);
-    for(int i=0; i<10;i++) {
-      (*treadVector)[i].join();
     }
 
-  }
-
-  void stop() {
-    this->isOpen = false;
-    close(this->server_socket);
-  }
-
-  int newSocket(int port) {
-    this->server_socket = socket(AF_INET, SOCK_STREAM, 0);;
-    if (server_socket==-1) {
-      cerr << "Could not create a socket" << endl;
-      return -1;
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    if (bind(server_socket, (struct sockaddr *) &address, sizeof(address))==-1) {
-      cerr << "Could not bind the socket to an IP" << endl;
-      return -2;
+    void stop() {
+      this->isOpen = false;
+      close(this->server_socket);
     }
 
-    if (listen(server_socket, 10)==-1) {
-      cerr << "Error during listening command" << endl;
-    } else {
-      this->isOpen = true;
-      cout << "waiting" << endl;
-    }
-    while (isOpen) {}
-  }
-
-  void start(ClientHandler *myHandler) {
-    this_thread::sleep_for(200ms);
-    //when server opened we can open client thread
-    while (!isOpen) {}
-    if (isOpen) {
-      //waiting till client connect
-//            timeout
-      struct timeval tv;
-      tv.tv_sec = 120;
-      setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv);
-     int  client_socket = accept(server_socket, (struct sockaddr *) &address, (socklen_t *) &address);
-      if (client_socket==-1) {
-        cerr << "Error accepting client" << endl;
-        liveCounter++;
-      } else {
-        cout << "client connected" << endl;
-        isOpen = true;
-        myHandler->handleClient(client_socket);; // need to check about arguments
-// current client is disconnecting
-        delete (myHandler);
-        cout << "client disconnected" << endl;
+    int newSocket(int port, vector<ClientHandler*>* handlers) {
+      this->server_socket = socket(AF_INET, SOCK_STREAM, 0);;
+      if (server_socket==-1) {
+        cerr << "Could not create a socket" << endl;
+        return -1;
       }
-      liveCounter++;
-      while (liveCounter < 10) {}
+      address.sin_family = AF_INET;
+      address.sin_addr.s_addr = INADDR_ANY;
+      address.sin_port = htons(port);
+
+      if (bind(server_socket, (struct sockaddr *) &address, sizeof(address))==-1) {
+        cerr << "Could not bind the socket to an IP" << endl;
+        return -2;
+      }
+
+      if (listen(server_socket, 10)==-1) {
+        cerr << "Error during listening command" << endl;
+      } else {
+        this->isOpen = true;
+        cout << "waiting" << endl;
+      }
+      start(handlers);
+      for(int i=0; i<10;i++) {
+        (*treadVector)[i].join();
+      }
+//    while (isOpen) {}
+    }
+
+    void start(vector<ClientHandler*>* handlers) {
+      //when server opened we can open client thread
+      while (!isOpen) {}
+      while (liveCounter<10) {
+        //waiting till client connect
+//            timeout
+        struct timeval tv;
+        tv.tv_sec = 120;
+        setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv);
+        int  client_socket = accept(server_socket, (struct sockaddr *) &address, (socklen_t *) &address);
+        if (client_socket==-1) {
+          cerr << "Error accepting client" << endl;
+          liveCounter++;
+        } else {
+          cout << "client connected" << endl;
+          isOpen = true;
+//        myHandler->handleClient(client_socket);; // need to check about arguments
+          ClientHandler *newHandler = (*handlers)[liveCounter]->clone();
+          treadVector->push_back(thread(&ClientHandler::handleClient, newHandler, std::ref(client_socket)));
+
+// current client is disconnecting
+//          delete (newHandler);
+          cout << "client disconnected" << endl;
+        }
+        liveCounter++;
+        this_thread::sleep_for(200ms);
+      }
       stop();
     }
-  }
-  ~MyParallelServer() override {
-    delete treadVector;
-  }
-  //once server has been opened handel client by calling myHandler.handleClient
+    ~MyParallelServer() override {
+      delete treadVector;
+    }
+    //once server has been opened handel client by calling myHandler.handleClient
 };
 
 #endif //EX4_MyParallelServer_H
